a2600b5f614cd9b928e6b4210fb24afac954b7f7
yuexin <610696148@qq.com>
2019-12-06 20:05:07 +0800

    add：golang学习

--- model/utils/struct.go
+++ model/utils/struct.go
@@ -3,6 +3,7 @@ package commutils
 import (
 	"gopkg.in/jeevatkm/go-model.v1"
 	"reflect"
+	"strings"
 )
 
 func IsZero(v interface{}) bool {
@@ -17,4 +18,19 @@ func IsZeroOrNil(v interface{}) bool {
 		return true
 	}
 	return model.IsZero(v)
+}
+
+// 结构体转换成Map;
+func StructToMap(obj interface{}) map[string]interface{} {
+	var data = make(map[string]interface{})
+	s := reflect.ValueOf(obj).Elem()
+	typeOfT := s.Type()
+	for i := 0; i < s.NumField(); i++ {
+		if strings.HasPrefix(typeOfT.Field(i).Name, "XXX_") {
+			continue
+		}
+		f := s.Field(i)
+		data[typeOfT.Field(i).Name] = f.Interface()
+	}
+	return data
 }
